09b6d3878c401c1fc42d6f3aa8b22229d09c406a
Aniket D Giri <aniket.giri@provakil.com>
2024-08-09 07:45:34 +0530

    feat: Next Permutation better approach

--- Arrays/Medium/Next-permutations.cpp
+++ Arrays/Medium/Next-permutations.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -30,12 +31,53 @@ public:
         int n = nums.size();
         PrintAllPermutationUsingRecursion(nums, 0, n);
     }
+
+    void findNextGreateElement(vector<int> &nums, int start)
+    {
+        int initialEle = nums[start - 1];
+        int nextGreaterNum = nums[start];
+        int nextGreaterIndex = start;
+        for (int i = start + 1; i < nums.size(); i++)
+        {
+            if (nums[i] > initialEle and nums[i] < nextGreaterNum)
+            {
+                nextGreaterNum = nums[i];
+                nextGreaterIndex = i;
+            }
+        }
+        swap(nums[start - 1], nums[nextGreaterIndex]);
+        sort(nums.begin() + start, nums.end());
+    }
+
+    // Optmial Approach
+    void nextPermutation(vector<int> &nums)
+    {
+        int n = nums.size() - 1;
+        int i = n;
+        while (i > 0 and nums[i] < nums[i - 1])
+        {
+            i--;
+        }
+        if (i == 0)
+        {
+            sort(nums.begin(), nums.end());
+            return;
+        }
+        findNextGreateElement(nums, i);
+    }
 };
 
 int main()
 {
-    vector<int> arr = {1, 2, 3, 4};
+    vector<int> arr = {4, 1, 3, 2};
     Solution obj;
-    obj.Approach1(arr);
+    obj.nextPermutation(arr);
+
+    cout << "Next Permutation is:";
+    for (const auto i : arr)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
     return 0;
 }
