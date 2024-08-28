84f9600163e476193577961b9ed28c2cfdf755a3
AnkDGiri777 <aniketgiri770@gmail.com>
2024-08-28 08:37:21 +0530

    feat: Variation of Count Inversion

---
+++ Arrays/Hard/Reverse-pairs.cpp
@@ -0,0 +1,140 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+private:
+    int totalInversion = 0;
+
+public:
+    void CountInversions(vector<int> &arr1, vector<int> &arr2)
+    {
+        int len1 = arr1.size();
+        int len2 = arr2.size();
+
+        int i = 0, j = 0;
+        while (i < len1 and j < len2)
+        {
+            if (static_cast<long long>(arr1[i]) > 2 * static_cast<long long>(arr2[j])) // This is used for handling intergers which are very large.
+            {
+
+                totalInversion += (len1 - i);
+                j++;
+            }
+            else
+            {
+                i++;
+            }
+        }
+    }
+
+    void Merge(vector<int> &nums, int low, int mid, int high)
+    {
+        vector<int> arr1(mid - low + 1, 0);
+        vector<int> arr2(high - mid, 0);
+
+        int temp = low;
+        int idx = 0;
+        while (temp <= mid)
+        {
+            arr1[idx] = nums[temp];
+            temp++;
+            idx++;
+        }
+
+        temp = mid + 1;
+        idx = 0;
+        while (temp <= high)
+        {
+            arr2[idx] = nums[temp];
+            temp++;
+            idx++;
+        }
+        CountInversions(arr1, arr2);
+
+        temp = low;
+        int i = 0;
+        int j = 0;
+        int n = mid - low + 1;
+        int m = high - mid;
+
+        while (i < n and j < m)
+        {
+            if (arr1[i] < arr2[j])
+            {
+                nums[temp] = arr1[i];
+                temp++;
+                i++;
+            }
+            else
+            {
+                nums[temp] = arr2[j];
+                j++;
+                temp++;
+            }
+        }
+
+        if (i < n)
+        {
+            while (i < n)
+            {
+                nums[temp] = arr1[i];
+                temp++;
+                i++;
+            }
+        }
+
+        if (j < m)
+        {
+            while (j < m)
+            {
+                nums[temp] = arr2[j];
+                j++;
+                temp++;
+                ;
+            }
+        }
+    }
+
+    void Partition(vector<int> &nums, int low, int high)
+    {
+        if (low == high)
+            return;
+        int mid = (low + high) / 2;
+        Partition(nums, low, mid);
+        Partition(nums, mid + 1, high);
+        Merge(nums, low, mid, high);
+    }
+
+    void OptimalApproach(vector<int> &nums)
+    {
+
+        int low = 0;
+        int high = nums.size() - 1;
+        Partition(nums, low, high);
+    }
+
+    int getTotalInversions()
+    {
+        return totalInversion;
+    }
+};
+
+int main()
+{
+    vector<int> arr = {2, 4, 3, 5, 1};
+
+    Solution obj;
+    obj.OptimalApproach(arr);
+
+    cout << "Sorted Array is:" << endl;
+    for (auto i : arr)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+    cout << "Total Inversions are:" << obj.getTotalInversions() << endl;
+    return 0;
+}
