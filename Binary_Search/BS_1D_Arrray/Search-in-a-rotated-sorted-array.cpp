067620ca1d4d4c63d637145445af2a9b6388deab
Aniket D Giri <aniket.giri@provakil.com>
2024-08-28 18:10:46 +0530

    feat: added code for rotated sorted array

---
+++ Binary_Search/BS_1D_Arrray/Search-in-a-rotated-sorted-array.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int OptimalApproach(vector<int> &nums, int target)
+    {
+    }
+};
+
+int main()
+{
+    vector<int> arr = {4, 5, 6, 7, 0, 1, 2};
+    return 0;
+}
