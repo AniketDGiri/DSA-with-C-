61008fa4cdf116b4596d13046876a78a61b937b6
AnkDGiri777 <aniketgiri770@gmail.com>
2024-08-30 08:38:52 +0530

    feat: added code for Single element in BS

--- Binary_Search/BS_1D_Arrray/Single-element-in-a-sorted-array.cpp
+++ Binary_Search/BS_1D_Arrray/Single-element-in-a-sorted-array.cpp
@@ -8,13 +8,44 @@ class Solution
 public:
     int OptimalApproach(vector<int> &nums)
     {
+        int n = nums.size();
+
+        if (n == 1)
+        {
+            return nums[0];
+        }
+
+        if (nums[0] != nums[1])
+            return nums[0];
+
+        if (nums[n - 1] != nums[n - 2])
+            return nums[n - 1];
+
+        int low = 1, high = n - 2;
+
+        while (low <= high)
+        {
+            int mid = (low + high) / 2;
+
+            if ((nums[mid] != nums[mid - 1]) and (nums[mid] != nums[mid + 1]))
+                return nums[mid];
+            else if ((mid % 2 == 1 and nums[mid - 1] == nums[mid]) or (mid % 2 == 0) and nums[mid + 1] == nums[mid])
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+        return -1;
     }
 };
 
 int main()
 {
-    vector<int> arr = {};
+    vector<int> arr = {1, 1, 2, 2, 3, 4, 4, 8, 8};
     Solution obj;
-    obj.OptimalApproach(arr);
+    cout << obj.OptimalApproach(arr) << endl;
     return 0;
 }
