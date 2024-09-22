4db960956e224a930190c091d8f5373ad84001d9
AnkDGiri777 <aniketgiri770@gmail.com>
2024-09-22 23:14:51 +0530

    feat: added code for Generate all powerset using bit manipulation approach

--- Recursion/Generate-all-possible-subsets.cpp
+++ Recursion/Generate-all-possible-subsets.cpp
@@ -28,14 +28,52 @@ public:
         FindPossibleSubset(0, n, temp, nums);
         return arr;
     }
+
+    vector<vector<int>> SubsetsApproach2(vector<int> &nums)
+    {
+        int n = nums.size();
+        long long powerSetSize = FindPowerSet(n);
+        for (long long j = 0; j < powerSetSize; j++)
+        {
+            vector<int> temp;
+            for (int i = 0; i < n; i++)
+            {
+                if ((j & (1 << i)) != 0)
+                {
+                    temp.push_back(nums[i]);
+                }
+            }
+            arr.push_back(temp);
+        }
+        return arr;
+    }
+    long long FindPowerSet(int x)
+    {
+        long long ans = 1;
+        int val = 2;
+        while (x != 0)
+        {
+            if (x % 2)
+            {
+                ans *= val;
+                x = x - 1;
+            }
+            else
+            {
+                val = val * val;
+                x = x / 2;
+            }
+        }
+        return ans;
+    }
 };
 
 int main()
 {
-    vector<int> nums = {1};
+    vector<int> nums = {1, 2, 3};
     Solution obj;
     vector<vector<int>> arr;
-    arr = obj.subsets(nums);
+    arr = obj.SubsetsApproach2(nums);
 
     cout << "All possible subsets are: " << endl;
     for (int i = 0; i < arr.size(); i++)
@@ -48,5 +86,8 @@ int main()
         cout << "]";
         cout << endl;
     }
+
+    // cout << obj.FindPowerSet(3) << endl;
+
     return 0;
 }
