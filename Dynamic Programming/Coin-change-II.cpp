ff8173f9788641c0a7ab20a9659c2542f3d92cf8
AnkDGiri777 <aniketgiri770@gmail.com>
2024-10-04 13:13:03 +0530

    added code for count of min coin ways using bottom up approach

--- Dynamic Programming/Coin-change-II.cpp
+++ Dynamic Programming/Coin-change-II.cpp
@@ -27,10 +27,68 @@ public:
         return leftCount + rightCount;
     }
 
+    int changeDP(int amount, vector<int> &coins)
+    {
+        int n = coins.size();
+        // return totalChangeWays(amount, coins, n);
+
+        vector<vector<int>> memo(n + 1, vector<int>(amount + 1, 0));
+
+        for (int i = 0; i <= n; i++)
+        {
+            memo[i][0] = 1;
+        }
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= amount; j++)
+            {
+                if (coins[i - 1] <= j)
+                {
+                    memo[i][j] = memo[i][j - coins[i - 1]] + memo[i - 1][j];
+                }
+                else
+                {
+                    memo[i][j] = memo[i - 1][j];
+                }
+            }
+        }
+        return memo[n][amount];
+    }
+
+    int changeUsingMemo(int amount, vector<int> &coins, vector<vector<int>> &memo, int n)
+    {
+        int leftCount = 0, rightCount = 0;
+
+        if (n == 0)
+        {
+            if (amount == 0)
+            {
+                return 1;
+            }
+            return 0;
+        }
+
+        if (memo[n][amount] != -1)
+        {
+            return memo[n][amount];
+        }
+        if (coins[n - 1] <= amount)
+        {
+            leftCount += changeUsingMemo(amount - coins[n - 1], coins, memo, n);
+        }
+        rightCount += changeUsingMemo(amount, coins, memo, n - 1);
+
+        return memo[n][amount] = leftCount + rightCount;
+    }
+
     int change(int amount, vector<int> &coins)
     {
         int n = coins.size();
-        return totalChangeWays(amount, coins, n);
+        // return totalChangeWays(amount, coins, n);
+
+        vector<vector<int>> memo(n + 1, vector<int>(amount + 1, -1));
+        return changeUsingMemo(amount, coins, memo, n);
     }
 };
 
