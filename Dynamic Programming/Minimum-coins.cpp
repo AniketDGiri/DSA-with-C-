38a2b818d350b67ab330df356afe333dc8a37545
AnkDGiri777 <aniketgiri770@gmail.com>
2024-10-04 11:05:06 +0530

    added code for count of min coin DP

--- Dynamic Programming/Minimum-coins.cpp
+++ Dynamic Programming/Minimum-coins.cpp
@@ -38,12 +38,40 @@ public:
         //     return -1;
         // }
         // return minCount;
+        int n = coins.size();
+
+        vector<vector<int>> memo(n + 1, vector<int>(amount + 1, 0));
+
+        for (int i = 0; i <= amount; i++)
+        {
+            memo[0][i] = INT_MAX - 1;
+        }
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= amount; j++)
+            {
+                if (coins[i - 1] <= j)
+                {
+                    memo[i][j] = min(1 + memo[i][j - coins[i - 1]], memo[i - 1][j]);
+                }
+                else
+                {
+                    memo[i][j] = memo[i - 1][j];
+                }
+            }
+        }
+        if (memo[n][amount] == INT_MAX || memo[n][amount] == INT_MAX - 1)
+        {
+            return -1;
+        }
+        return memo[n][amount];
     }
 };
 
 int main()
 {
-    vector<int> arr = {2};
+    vector<int> arr = {1, 2, 5};
     int amount = 11;
 
     Solution obj;
